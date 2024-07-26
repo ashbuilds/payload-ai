6c912594f1b4230c7afcd0b5f3b16d9465580e9c
ashbuilds <ashish.mishra41191@gmail.com>
2024-07-26 19:19:41 +0200

    [chore] fix endpoint url defaults

--- src/defaults.ts
+++ src/defaults.ts
@@ -3,6 +3,6 @@ export const PLUGIN_INSTRUCTIONS_TABLE = 'instructions'
 export const PLUGIN_INSTRUCTIONS_MAP_GLOBAL = `${PLUGIN_NAME}__${PLUGIN_INSTRUCTIONS_TABLE}_map`
 
 // Endpoint defaults
-export const PLUGIN_API_ENDPOINT_BASE = '/api/plugin-ai'
+export const PLUGIN_API_ENDPOINT_BASE = '/plugin-ai'
 export const PLUGIN_API_ENDPOINT_GENERATE = `${PLUGIN_API_ENDPOINT_BASE}/generate`
 export const PLUGIN_API_ENDPOINT_GENERATE_UPLOAD = `${PLUGIN_API_ENDPOINT_GENERATE}/upload`
