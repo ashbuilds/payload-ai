8f4c0f7817b059ea8519b0c93c2b888479d604ce
ash <ashish.mishra@eltemate.com>
2024-08-13 21:06:50 +0200

    [chore] try/catch on getAllVoices, if API key is not available

--- src/ai/models/elevenLabs/index.ts
+++ src/ai/models/elevenLabs/index.ts
@@ -1,11 +1,7 @@
 import type { File } from 'payload'
 
-import { usePayloadAPI } from '@payloadcms/ui'
-import payload from 'payload'
-
 import type { GenerationConfig } from '../../../types.js'
 
-import { PromptEditorField } from '../../../fields/PromptEditorField/PromptEditorField.js'
 import { SelectField } from '../../../fields/SelectField/SelectField.js'
 import { generateFileNameByPrompt } from '../../utils/generateFileNameByPrompt.js'
 import { generateVoice } from './generateVoice.js'
