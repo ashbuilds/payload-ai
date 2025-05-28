4728ac134c6e63435fa5fb55a87f5ad289c1cbf1
Nico Hülscher <25116822+eweren@users.noreply.github.com>
2025-05-28 17:36:32 +0200

    [feat] add support for globals

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,4 +1,4 @@
-import type { Config } from 'payload'
+import type { CollectionConfig, Config, GlobalConfig } from 'payload'
 
 import { deepMerge } from 'payload/shared'
 
@@ -45,106 +45,119 @@ const sponsorMessage = `
 
 const payloadAiPlugin =
   (pluginConfig: PluginConfig) =>
-  (incomingConfig: Config): Config => {
+    (incomingConfig: Config): Config => {
 
-    pluginConfig = { ...defaultPluginConfig, ...pluginConfig }
-    pluginConfig.generationModels = getGenerationModels(pluginConfig)
+      pluginConfig = { ...defaultPluginConfig, ...pluginConfig }
+      pluginConfig.generationModels = getGenerationModels(pluginConfig)
 
-    const isActivated = isPluginActivated(pluginConfig)
-    let updatedConfig: Config = { ...incomingConfig }
-    let collectionsFieldPathMap = {}
+      const isActivated = isPluginActivated(pluginConfig)
+      let updatedConfig: Config = { ...incomingConfig }
+      let collectionsFieldPathMap = {}
 
-    if (isActivated) {
-      const Instructions = instructionsCollection(pluginConfig)
-      // Inject editor schema to config, so that it can be accessed when /textarea endpoint will hit
-      const lexicalSchema = lexicalJsonSchema(pluginConfig.editorConfig?.nodes)
+      if (isActivated) {
+        const Instructions = instructionsCollection(pluginConfig)
+        // Inject editor schema to config, so that it can be accessed when /textarea endpoint will hit
+        const lexicalSchema = lexicalJsonSchema(pluginConfig.editorConfig?.nodes)
 
-      if (pluginConfig.debugging) {
-        Instructions.admin.hidden = false
-      }
+        if (pluginConfig.debugging) {
+          Instructions.admin.hidden = false
+        }
 
-      Instructions.admin.custom = {
-        ...(Instructions.admin.custom || {}),
-        [PLUGIN_NAME]: {
-          editorConfig: {
-            // Used in admin client for useObject hook
-            schema: lexicalSchema,
+        Instructions.admin.custom = {
+          ...(Instructions.admin.custom || {}),
+          [PLUGIN_NAME]: {
+            editorConfig: {
+              // Used in admin client for useObject hook
+              schema: lexicalSchema,
+            },
           },
-        },
-      }
+        }
 
-      const collections = [...(incomingConfig.collections ?? []), Instructions]
-      const { collections: collectionSlugs = [] } = pluginConfig
+        const collections = [...(incomingConfig.collections ?? []), Instructions]
+        const globals = [...(incomingConfig.globals ?? [])]
+        const { collections: collectionSlugs = [], globals: globalsSlugs = [] } = pluginConfig
 
-      const { components: { providers = [] } = {} } = incomingConfig.admin || {}
-      const updatedProviders = [
-        ...(providers ?? []),
-        {
-          path: '@ai-stack/payloadcms/client#InstructionsProvider',
-        },
-      ]
+        const { components: { providers = [] } = {} } = incomingConfig.admin || {}
+        const updatedProviders = [
+          ...(providers ?? []),
+          {
+            path: '@ai-stack/payloadcms/client#InstructionsProvider',
+          },
+        ]
 
-      incomingConfig.admin = {
-        ...(incomingConfig.admin || {}),
-        components: {
-          ...(incomingConfig.admin?.components ?? {}),
-          providers: updatedProviders,
-        },
-      }
+        incomingConfig.admin = {
+          ...(incomingConfig.admin || {}),
+          components: {
+            ...(incomingConfig.admin?.components ?? {}),
+            providers: updatedProviders,
+          },
+        }
 
-      const pluginEndpoints = endpoints(pluginConfig)
-      updatedConfig = {
-        ...incomingConfig,
-        collections: collections.map((collection) => {
-          if (collectionSlugs[collection.slug]) {
-            const { schemaPathMap, updatedCollectionConfig } = updateFieldsConfig(collection)
-            collectionsFieldPathMap = {
-              ...collectionsFieldPathMap,
-              ...schemaPathMap,
+        const pluginEndpoints = endpoints(pluginConfig)
+        updatedConfig = {
+          ...incomingConfig,
+          collections: collections.map((collection) => {
+            if (collectionSlugs[collection.slug]) {
+              const { schemaPathMap, updatedCollectionConfig } = updateFieldsConfig(collection)
+              collectionsFieldPathMap = {
+                ...collectionsFieldPathMap,
+                ...schemaPathMap,
+              }
+              return updatedCollectionConfig as CollectionConfig
             }
-            return updatedCollectionConfig
-          }
 
-          return collection
-        }),
-        endpoints: [
-          ...(incomingConfig.endpoints ?? []),
-          pluginEndpoints.textarea,
-          pluginEndpoints.upload,
-          fetchFields(pluginConfig.access),
-        ],
-        i18n: {
-          ...(incomingConfig.i18n || {}),
-          translations: {
-            ...deepMerge(translations, incomingConfig.i18n?.translations ?? {}),
+            return collection
+          }),
+          endpoints: [
+            ...(incomingConfig.endpoints ?? []),
+            pluginEndpoints.textarea,
+            pluginEndpoints.upload,
+            fetchFields(pluginConfig.access),
+          ],
+          globals: globals.map((global) => {
+            if (globalsSlugs.includes(global.slug)) {
+              const { schemaPathMap, updatedCollectionConfig } = updateFieldsConfig(global)
+              collectionsFieldPathMap = {
+                ...collectionsFieldPathMap,
+                ...schemaPathMap,
+              }
+              return updatedCollectionConfig as GlobalConfig;
+            }
+
+            return global
+          }),
+          i18n: {
+            ...(incomingConfig.i18n || {}),
+            translations: {
+              ...deepMerge(translations, incomingConfig.i18n?.translations ?? {}),
+            },
           },
-        },
-      }
-    }
-
-    updatedConfig.onInit = async (payload) => {
-      if (incomingConfig.onInit) await incomingConfig.onInit(payload)
-
-      if (!isActivated) {
-        payload.logger.warn(`— AI Plugin: Not activated. Please verify your environment keys.`)
-        return
+        }
       }
 
-      await init(payload, collectionsFieldPathMap, pluginConfig)
-        .catch((error) => {
-          console.error(error)
-          payload.logger.error(`— AI Plugin: Initialization Error: ${error}`)
-        })
-        .finally(() => {
-          if (!pluginConfig.disableSponsorMessage) {
-            setTimeout(() => {
-              payload.logger.info(sponsorMessage)
-            }, 3000)
-          }
-        })
-    }
+      updatedConfig.onInit = async (payload) => {
+        if (incomingConfig.onInit) await incomingConfig.onInit(payload)
 
-    return updatedConfig
-  }
+        if (!isActivated) {
+          payload.logger.warn(`— AI Plugin: Not activated. Please verify your environment keys.`)
+          return
+        }
+
+        await init(payload, collectionsFieldPathMap, pluginConfig)
+          .catch((error) => {
+            console.error(error)
+            payload.logger.error(`— AI Plugin: Initialization Error: ${error}`)
+          })
+          .finally(() => {
+            if (!pluginConfig.disableSponsorMessage) {
+              setTimeout(() => {
+                payload.logger.info(sponsorMessage)
+              }, 3000)
+            }
+          })
+      }
+
+      return updatedConfig
+    }
 
 export { payloadAiPlugin }
