52c8735af88ef14d5a6a19863a798351d016ab9d
panrafal <rl@stamina.pl>
2025-09-11 15:38:53 +0200

    Expose config-related types

--- src/exports/types.ts
+++ src/exports/types.ts
@@ -1,2 +1,15 @@
 export type { payloadAiPlugin } from '../index.ts'
-export type { GenerationConfig, GenerationModel, PluginConfig } from '../types.ts'
+export type { 
+  GenerationConfig, 
+  GenerationModel,
+  PluginConfig, 
+  PluginConfigAccess,
+  PluginConfigMediaUploadFunction,
+  PluginOptions,
+  PromptField,
+  PromptFieldGetterContext,
+  SeedPromptData,
+  SeedPromptFunction,
+  SeedPromptOptions,
+  SeedPromptResult,
+} from '../types.ts'
